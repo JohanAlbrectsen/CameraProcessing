2b7c45a3e652caaee4cff7300e98c43da3d074b0
Johan Albrectsen <johan@albrectsen.com>
2024-10-06 12:00:02 +0200

    Initial Commit

---
+++ CameraProcessingTests/CameraProcessingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraProcessingTests.swift
+//  CameraProcessingTests
+//
+//  Created by Johan Albrectsen on 06/10/2024.
+//
+
+import Testing
+@testable import CameraProcessing
+
+struct CameraProcessingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
